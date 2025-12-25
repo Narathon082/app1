7ae202a9b9992d3c6f540cb0ad351a145602f847
Narathon082 <s6703052411040@email.kmutnb.ac.th>
2025-12-25 11:10:38 +0700

    Function Component
    Function Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-import './style.css';
+import React from 'react';
+import { Header,Content,Footer } from './func-components';
+
 
 function App() {
   return (
-    <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
-    </>
-  );
+    [<Header/>,<Content/>,<Footer/>]
+  )
 }
 
 export default App;
