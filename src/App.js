f2cc2bea2fc89f2d9085f72c9f130a2a33643df5
Narathon082 <s6703052411040@email.kmutnb.ac.th>
2026-01-29 10:55:37 +0700

    Centralized storage with Context
    Context
    content2
    header2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import React from 'react'
 import { userContext } from './context';
-import Content from './context-content';
+import Content from './context-content2';
+import Header2 from './context-header2';
 
 function App() {
+  let [user, setUser] = React.useState('');
   return (
-    <userContext.Provider value="Tom Jerry">
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
       <Content/>
     </userContext.Provider>
   );
