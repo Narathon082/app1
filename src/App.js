796abdb03c462fd308631db48f1aa98035bfb255
Narathon082 <s6703052411040@email.kmutnb.ac.th>
2026-02-12 10:05:36 +0700

    Router And Navlink.css
    Router And Navlink.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 //import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import { userContext } from './context';
-import Content from './context-content2';
-import Header2 from './context-header2';
+import Router2 from './router2';
 
-function App() {
-  let [user, setUser] = React.useState('');
-  return (
-    <userContext.Provider value={[user,setUser]}>
-      <Header2/>
-      <Content/>
-    </userContext.Provider>
-  );
+export default function App() {
+
+  return <Router2/>;
 }
-
-export default App;
