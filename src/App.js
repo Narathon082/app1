519ef2c95f90bf6eae55f87f74613b98867b128a
Narathon082 <s6703052411040@email.kmutnb.ac.th>
2026-02-19 10:27:41 +0700

    Route
    page home,member,products,router3 and add css edit navlink

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 import './App.css';
 import React from 'react'
 import Router2 from './router2';
+import Router3 from './router3';
 
 export default function App() {
 
-  return <Router2/>;
+  return <Router3/>;
 }
