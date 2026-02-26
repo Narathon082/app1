0fe1a4929e8fb100c344b22c5f793adf5c8981ae
Narathon082 <s6703052411040@email.kmutnb.ac.th>
2026-02-26 10:13:42 +0700

    Update form-select-file.js

--- src/form-select-file.js
+++ src/form-select-file.js
@@ -18,7 +18,7 @@ export default function FormSelectFile() {
         }
     for (let f of inputFile.current.files) {
             if (f.size > maxSize * 1000) {
-                alert(`ขนาดของไฟล์เกินต้องไม่เกิด ${maxSize} KB`)
+                alert(`ขนาดของไฟล์ต้องไม่เกิด ${maxSize} KB`)
                 return
             }
         }
